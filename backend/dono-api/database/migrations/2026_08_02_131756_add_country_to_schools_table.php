b90769fb30c5c5ac37f638a3a7911df12bbe6960
DONO De Creator <josephaondover96@gmail.com>
2026-08-02 14:22:51 +0100

    Add country string column to schools table

---
+++ backend/dono-api/database/migrations/2026_08_02_131756_add_country_to_schools_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            if (!Schema::hasColumn('schools', 'country')) {
+                $table->string('country')->nullable();
+            }
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->dropColumn('country');
+        });
+    }
+};
+
