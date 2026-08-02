7063eca8285d436c0a45d69c7195392dbc11a147
DONO De Creator <josephaondover96@gmail.com>
2026-08-02 12:10:44 +0100

    Add countries table setup, migration, and school onboarding form

--- backend/dono-api/app/Http/Controllers/Api/CountryController.php
+++ backend/dono-api/app/Http/Controllers/Api/CountryController.php
@@ -3,47 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Country;
 use Illuminate\Http\Request;
 
 class CountryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'success' => true,
+            'data' => Country::all()
+        ]);
     }
 }
+
