2d38e1c419811ddecda7dc9b4b3b55c9b0874cb0
DONO De Creator <josephaondover96@gmail.com>
2026-08-03 08:27:17 +0100

    Add BrowserRouter to main.jsx entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import "./App.css";
@@ -16,9 +17,11 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ErrorBoundary>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
 );
